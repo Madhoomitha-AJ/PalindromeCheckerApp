4ed8a384df2bcb590a4d0e2eb23c5d32a378b1e1
Madhoomitha-AJ <madhoomitha0904@gmail.com>
2026-03-02 15:09:31 +0530

    UC1:Application Entry & Welcome Message

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+//usecase1:welcomepage
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to palindrome chechker app management system");
+        System.out.println("-------------------");
+    }
+}
